a301bf1f6084d0a027ceb1e1d046326643a15bbd
Pedro Paulo <pedro.paulo@b2mlportal.com.br>
2021-03-29 19:28:36 -0300

    Autenticação com Next Auth Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  const [session] = useSession();
+  console.log(session);
+  return session ? (
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signOut()}
+    >
       <FaGithub color="#40b361" />
-      Pedro Paulo
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
