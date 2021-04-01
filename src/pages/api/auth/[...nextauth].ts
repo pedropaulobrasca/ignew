0a85903d5d499a2ed15a9adbd2b2cfff497d7027
Pedro Paulo <pedro.paulo@b2mlportal.com>
2021-03-31 21:00:26 -0300

    Salvando usuário no Banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,19 @@ export default NextAuth({
       scope: 'read:users',
     }),
   ],
+  jwt: {
+    signingKey: process.env.JWT_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        return true;
+      } catch (error) {
+        return false;
+      }
+      return true;
+    },
+  },
 });
