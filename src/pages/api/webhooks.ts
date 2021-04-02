b9f96ba8fe1cdc358042b518356b79a347f65c70
Pedro Paulo <pedro.paulo@b2mlportal.com>
2021-04-02 00:08:59 -0300

    Salvando dados do evento

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -40,7 +41,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('Received event', event);
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            );
+            break;
+
+          default:
+            throw new Error('Unhandled event.');
+        }
+      } catch (error) {
+        return res.json({ error: 'Webhook handler failed.' });
+      }
     }
 
     res.json({ received: true });
