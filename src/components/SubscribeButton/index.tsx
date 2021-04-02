e30d85817aff008ed1f3f9dc9cfd7d425f869cae
Pedro Paulo <pedro.paulo@b2mlportal.com>
2021-04-01 22:07:12 -0300

    Gerando sessão de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { useSession, signIn } from 'next-auth/client';
+import { stripe } from '../../services/stripe';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +7,23 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+  }
+
+  // criação da checkout session
+  stripe.checkout.sessions.create;
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
